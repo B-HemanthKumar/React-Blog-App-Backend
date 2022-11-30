c0d739bc090f286dbba26519c6cd4c9f8559dc97
B-HemanthKumar <hemanthpandu328@gmail.com>
2022-11-30 23:44:12 +0530

    first commit

---
+++ controllers/user-controller.js
@@ -0,0 +1,45 @@
+import User from "../model/User";
+
+
+export const getAllUser = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!users) {
+    return res.status(404).json({ message: "No Users Found" });
+  }
+  return res.status(200).json({ users });
+};
+
+export const signup = async (req, res, next) => {
+  const { name, email } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err);
+  }
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ message: "User Exists!" });
+  }
+ 
+
+  const user = new User({
+    name,
+    email,
+    blogs: [],
+  });
+
+  try {
+    await user.save();
+  } catch (err) {
+    return console.log(err);
+  }
+  return res.status(201).json({ user });
+};
+
